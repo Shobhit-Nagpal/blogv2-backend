337cf8bbca040c7fd62c9242d7fff894b58ba566
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-02-11 17:42:54 +0530

    Corrected logic of post save controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -9,178 +9,177 @@ const SERVICE_KEY = process.env.SERVICE_KEY;
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 const supabase = createClient(PROJECT_URL, SERVICE_KEY);
 
-exports.posts_list_get = asyncHandler(async(req, res, next) => {
-    const { data, error } = await supabase
-        .from("posts")
-        .select()
-        .order("created_at", {ascending: false})
-        .eq("is_published", true);
+exports.posts_list_get = asyncHandler(async (req, res, next) => {
+  const { data, error } = await supabase
+    .from("posts")
+    .select()
+    .order("created_at", { ascending: false })
+    .eq("is_published", true);
 
-    if (error) {
-        return res.status(500).json({error: error});
-    } else {
-        return res.status(200).json(data);
-    }
+  if (error) {
+    return res.status(500).json({ error: error });
+  } else {
+    return res.status(200).json(data);
+  }
 });
 
-exports.post_get = asyncHandler(async(req, res, next) => {
-    const { id } = req.params;
+exports.post_get = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
 
-    const { data, error } = await supabase
-        .from("posts")
-        .select()
-        .eq("id", id);
+  const { data, error } = await supabase.from("posts").select().eq("id", id);
 
-    if (error) {
-        return res.status(500).json({error: error});
-    } else {
-        return res.status(200).json(data);
-    }
+  if (error) {
+    return res.status(500).json({ error: error });
+  } else {
+    return res.status(200).json(data);
+  }
 });
 
 exports.post_publish = [
-    body("title", "Title should be more than 4 characters").isLength({ min: 4 }).escape(),
-    body("content", "Content should be more than 4 characters").isLength({ min: 4 }).escape(),
-    asyncHandler(async(req, res, next) => {
+  body("title", "Title should be more than 4 characters")
+    .isLength({ min: 4 })
+    .escape(),
+  body("content", "Content should be more than 4 characters")
+    .isLength({ min: 4 })
+    .escape(),
+  asyncHandler(async (req, res, next) => {
     const { title, content } = req.body;
 
     const { error } = await supabase
-        .from("posts")
-        .insert({title: title, content: content, is_published: true});
+      .from("posts")
+      .insert({ title: title, content: content, is_published: true });
 
     if (error) {
-        return res.status(500).json({error: error});
+      return res.status(500).json({ error: error });
     } else {
-        return res.status(201).json({"message": "Created post"});
+      return res.status(201).json({ message: "Created post" });
     }
-})
-]
+  }),
+];
 
 exports.post_save = [
-    body("title", "Title should be more than 4 characters").isLength({ min: 4 }).escape(),
-    body("content", "Content should be more than 4 characters").isLength({ min: 4 }).escape(),
-    asyncHandler(async(req, res, next) => {
+  body("title", "Title should be more than 4 characters")
+    .isLength({ min: 4 })
+    .escape(),
+  body("content", "Content should be more than 4 characters")
+    .isLength({ min: 4 })
+    .escape(),
+  asyncHandler(async (req, res, next) => {
     const { id, title, content, is_published } = req.body;
 
-    const { data, err } = await supabase
+    if (id === null) {
+      const { error } = await supabase
         .from("posts")
-        .select()
-        .eq("id", id)
+        .insert({ title: title, content: content, is_published: false });
 
-    if (err) {
-        return res.status(500).json({error: err});
+      if (error) {
+        return res.status(500).json({ error: error });
+      } else {
+        return res.status(201).json({ message: "Created post" });
+      }
     } else {
-        const rowCount = data.length;
+      const { error } = await supabase
+        .from("posts")
+        .update({
+          title: title,
+          content: content,
+          updated_at: new Date(),
+          is_published: is_published,
+        })
+        .eq("id", id);
 
-        //Create post if does not exist else update old post
-        if (rowCount === 0) {
+      if (error) {
+        return res.status(500).json({ error: error });
+      }
 
-            const { error } = await supabase
-                .from("posts")
-                .insert({title: title, content: content, is_published: false});
-
-            if (error) {
-                return res.status(500).json({error: error});
-            } else {
-                return res.status(201).json({"message": "Created post"});
-            }
-
-        } else {
-
-            const { error } = await supabase
-                .from("posts")
-                .update({title: title, content: content, updated_at: new Date(), is_published: is_published})
-                .eq("id", id);
-
-            if (error) {
-                return res.status(500).json({error: error});
-            }
-
-            return res.status(200).json({"message": "Post updated!"});
-        }
+      return res.status(200).json({ message: "Post updated!" });
     }
-
-})
-]
+  }),
+];
 
 exports.post_update = [
-    body("title", "Title should be more than 4 characters").isLength({ min: 4 }).escape(),
-    body("content", "Content should be more than 4 characters").isLength({ min: 4 }).escape(),
-    asyncHandler(async(req, res, next) => {
+  body("title", "Title should be more than 4 characters")
+    .isLength({ min: 4 })
+    .escape(),
+  body("content", "Content should be more than 4 characters")
+    .isLength({ min: 4 })
+    .escape(),
+  asyncHandler(async (req, res, next) => {
     const { id, title, content, is_published } = req.body;
     const token = req.headers.authorization.split(" ")[1];
 
     if (token === undefined) {
-        return res.status(401).json({"message": "Not authorized"});
+      return res.status(401).json({ message: "Not authorized" });
     }
 
-    jwt.verify(token, JWT_ACCESS_SECRET, async function(err, info) {
-        if (err) {
-            return res.status(500).json({error: err});
-        }
+    jwt.verify(token, JWT_ACCESS_SECRET, async function (err, info) {
+      if (err) {
+        return res.status(500).json({ error: err });
+      }
 
-        const { error } = await supabase
+      const { error } = await supabase
         .from("posts")
-        .update({title: title, content: content, updated_at: new Date(), is_published: is_published})
+        .update({
+          title: title,
+          content: content,
+          updated_at: new Date(),
+          is_published: is_published,
+        })
         .eq("id", id);
 
-        if (error) {
-            return res.status(500).json({error: error});
-        }
+      if (error) {
+        return res.status(500).json({ error: error });
+      }
 
-        return res.status(200).json({"message": "Post updated!"});
+      return res.status(200).json({ message: "Post updated!" });
     });
-})
-]
+  }),
+];
 
-exports.post_delete = asyncHandler(async(req, res, next) => {
-    const { id } = req.body;
-    const token = req.headers.authorization.split(" ")[1];
-    
-    if (token === undefined) {
-        return res.status(401).json({error: "Not authorized"});
+exports.post_delete = asyncHandler(async (req, res, next) => {
+  const { id } = req.body;
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (token === undefined) {
+    return res.status(401).json({ error: "Not authorized" });
+  }
+
+  jwt.verify(token, JWT_ACCESS_SECRET, async function (err, info) {
+    if (err) {
+      return res.status(500).json({ error: err });
     }
 
-    jwt.verify(token, JWT_ACCESS_SECRET, async function(err, info) {
-        if (err) {
-            return res.status(500).json({error: err});
-        }
+    const { error } = await supabase.from("posts").delete().eq("id", id);
 
-        const { error } = await supabase
-        .from("posts")
-        .delete()
-        .eq("id", id);
+    if (error) {
+      return res.status(500).json({ error: error });
+    }
 
-        if (error) {
-            return res.status(500).json({error: error});
-        }
-
-        return res.status(200).json({"message": "Post deleted!"});
-    });
+    return res.status(200).json({ message: "Post deleted!" });
+  });
 });
 
-exports.dashboard_get = asyncHandler(async(req, res, next) => {
+exports.dashboard_get = asyncHandler(async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
 
-    const token = req.headers.authorization.split(" ")[1];
+  if (token === undefined) {
+    return res.status(401).json({ message: "Not authorized"`` });
+  }
 
-    if (token === undefined) {
-        return res.status(401).json({"message": "Not authorized"``});
+  jwt.verify(token, JWT_ACCESS_SECRET, async function (err, info) {
+    if (err) {
+      return res.status(500).json({ error: err });
     }
 
-    jwt.verify(token, JWT_ACCESS_SECRET, async function(err, info) {
-        if (err) {
-            return res.status(500).json({error: err});
-        }
+    const { data, error } = await supabase
+      .from("posts")
+      .select()
+      .order("created_at", { ascending: false });
 
-        const { data, error } = await supabase
-        .from("posts")
-        .select()
-        .order("created_at", {ascending: false});
+    if (error) {
+      return res.status(500).json({ error: error });
+    }
 
-        if (error) {
-            return res.status(500).json({error: error});
-        }
-
-        return res.status(200).json(data);
-    });
+    return res.status(200).json(data);
+  });
 });
